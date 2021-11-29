ae18ac2e03ae6b31eadccd95657725f8066691f8
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2021-11-29 17:13:03 +0800

    default

--- main.py
+++ main.py
@@ -11,29 +11,29 @@ from torch.autograd import Variable
 from dataloader import MnistBags
 from model import Attention, GatedAttention
 
-class ECELoss(nn.Module):
-    def __init__(self, n_bins=15):
-        super(ECELoss, self).__init__()
-        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
-        self.bin_lowers = bin_boundaries[:-1]
-        self.bin_uppers = bin_boundaries[1:]
+# class ECELoss(nn.Module):
+#     def __init__(self, n_bins=15):
+#         super(ECELoss, self).__init__()
+#         bin_boundaries = torch.linspace(0, 1, n_bins + 1)
+#         self.bin_lowers = bin_boundaries[:-1]
+#         self.bin_uppers = bin_boundaries[1:]
 
-    def forward(self, logits, labels):
-        softmaxes = F.softmax(logits, dim=1)
-        confidences, predictions = torch.max(softmaxes, 1)
-        accuracies = predictions.eq(labels)
+#     def forward(self, logits, labels):
+#         softmaxes = F.softmax(logits, dim=1)
+#         confidences, predictions = torch.max(softmaxes, 1)
+#         accuracies = predictions.eq(labels)
 
-        ece = torch.zeros(1, device=logits.device)
-        for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
-            # Calculated |confidence - accuracy| in each bin
-            in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
-            prop_in_bin = in_bin.float().mean()
-            if prop_in_bin.item() > 0:
-                accuracy_in_bin = accuracies[in_bin].float().mean()
-                avg_confidence_in_bin = confidences[in_bin].mean()
-                ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+#         ece = torch.zeros(1, device=logits.device)
+#         for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
+#             # Calculated |confidence - accuracy| in each bin
+#             in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
+#             prop_in_bin = in_bin.float().mean()
+#             if prop_in_bin.item() > 0:
+#                 accuracy_in_bin = accuracies[in_bin].float().mean()
+#                 avg_confidence_in_bin = confidences[in_bin].mean()
+#                 ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
 
-        return ece
+#         return ece
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch MNIST bags Example')
@@ -100,7 +100,7 @@ if args.cuda:
 
 optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.999), weight_decay=args.reg)
 
-ece = ECELoss()
+# ece = ECELoss()
 
 def train(epoch):
     model.train()
